315defd53ddadc46d768ecba30e028a1ce920c73
Joris van Vugt <jorisvan.vugt@student.ru.nl>
2016-05-24 23:27:58 +0200

    Change learning rate

--- run.py
+++ run.py
@@ -22,7 +22,7 @@ def run_single_id(run_id, show_progress=True):
     print 'starting run_id: ', run_id
     reward = 0
     successes = 0
-    model = ContextualThompsonModel(alpha=.01, beta=.01)
+    model = ContextualThompsonModel(alpha=.1, beta=.1)
     # mean_reward = np.zeros(REQUEST_NUMBERS / 100)
     for rn in range_func(REQUEST_NUMBERS):
         # if rn % 100 == 0:
